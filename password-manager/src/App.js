f697a4d8410786e56174f5d9f44fd68ee6a043f5
Ethan Barr <ethanwbarr@btinternet.com>
2022-12-30 23:37:29 +0000

    v0.0.2 - Start

--- password-manager/src/App.js
+++ password-manager/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PasswordGenerator from './Components/generator';
+import PasswordManager from './Components/manager';
 import Wedget from './Components/Wedget';
 
 function App() {
@@ -15,6 +16,14 @@ function App() {
         description="Move slider to change 
         number of charaters">
           <PasswordGenerator/>
+        </Wedget>
+      </div>
+
+      <div className='PasswordManager'>
+        <Wedget
+        title="Password Manager"
+        description="All your passwords saved here">
+          <PasswordManager/>
         </Wedget>
       </div>
     </div>
